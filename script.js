dac14dda01e4057fdb51ab70dd449077a3943e77
ysabri742-art <ysabri742@gmail.com>
2025-10-10 22:47:52 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 function startQuickMock() {
   localStorage.setItem("mode", "quick");
+  localStorage.removeItem("section");
+  localStorage.removeItem("returnTo");
+  localStorage.removeItem("questions");
+  localStorage.removeItem("timeLeft");
+  // مسح بيانات المراجعة القديمة
+  localStorage.removeItem("reviewQuestions"); 
   window.location.href = "quiz.html";
 }
 
 function startRealMock() {
   localStorage.setItem("mode", "real");
   localStorage.setItem("section", "1");
+  localStorage.removeItem("returnTo");
+  localStorage.removeItem("questions");
+  localStorage.removeItem("timeLeft");
+  // مسح بيانات المراجعة القديمة
+  localStorage.removeItem("reviewQuestions"); 
   window.location.href = "quiz.html";
 }
